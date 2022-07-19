9e8ac2817730c2d4acceb37581db0997d2d33d88
riorichard98 <riorichard12@gmail.com>
2022-07-19 21:54:34 +0700

    MUI and react query done

--- pixcels-mini-project/src/App.js
+++ pixcels-mini-project/src/App.js
@@ -1,23 +1,56 @@
-import logo from './logo.svg';
 import './App.css';
+import { useQuery } from "react-query";
+import axios from 'axios';
+import PostCard from './components/PostCard';
+// import AlignContent from './components/testFlex';
+import Box from '@mui/material/Box';
+
+const query = `query(
+  $options: PageQueryOptions
+) {
+  posts(options: $options) {
+    data {
+      id
+      title
+    }
+    meta {
+      totalCount
+    }
+  }
+}
+`
 
 function App() {
+  const { data, status } = useQuery("users", async () => {
+    const { data } = await axios.post('https://graphqlzero.almansi.me/api', {
+      query,
+      variables: {
+        "options": {
+          "paginate": {
+            "page": 1,
+            "limit": 10
+          }
+        }
+      }
+    }, {
+      "headers": { "content-type": "application/json" }
+    })
+    return data.data.posts.data
+  });
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {status === "error" && <p>Error fetching data</p>}
+      {status === "loading" && <p>Fetching data...</p>}
+      {status === "success" && (
+        <div style={{
+          display: "flex",
+          "flex-wrap":" wrap"
+        }}>
+          {data.map((post) => (
+            <PostCard></PostCard>
+          ))}
+        </div>
+      )}
     </div>
   );
 }
